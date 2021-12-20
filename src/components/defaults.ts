8987f594efe4e5958c6c5fba3f0a296907e8f8bb
kriskros341 <krzysztofczuba884@gmail.com>
2021-12-20 22:21:31 +0100

    now I know how to do this!

--- src/components/defaults.ts
+++
@@ -1,8 +0,0 @@
-const defaults = {
-	remoteUser: "root",
-	ipAddress: "192.168.1.21",
-	connection: {isConnected: false, isStarted: false},
-	options: {temperature: 50},
-}
-
-export default defaults
--- src/test.py
+++
@@ -1,8 +0,0 @@
-from time import sleep
-import sys
-x = 0
-while True:
-    sleep(1)
-    x+=1
-    sys.stdout.write(str(x))
-    sys.stdout.flush()
